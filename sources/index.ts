99c5b4594182dcb97104763fcd6ed7ae220f218a
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2019-01-18 14:18:38 -0500

    1.0.3: Finx index.ts

--- sources/index.ts
+++ sources/index.ts
@@ -4,11 +4,3 @@ export {
     CallConfig,
     Event
 } from './Portalize';
-
-import { Portalize } from './Portalize';
-
-Portalize.get.setPortal('./portal');
-Portalize.get.setModuleName('test');
-
-//Portalize.get.add('caca', {caca: 'caca'}, {desc: 'lol'});
-Portalize.get.clean();
